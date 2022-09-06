527a3bd493473f8ac708ce1f35fb28362e7ee384
Girzo <52513552+Giunossauro@users.noreply.github.com>
2022-09-06 19:39:44 -0300

    🔨 Refactoring

--- main.cpp
+++ main.cpp
@@ -2,43 +2,46 @@
 #include <ctime>
 
 int main() {
-    uint16_t foo[1000], bar[1000];
-    uint32_t startTime = clock(), elapsedTime = 0;
-    uint64_t comparisonsCounter = 0, coutMarkCounter = 0;
+	uint16_t foo[1000], bar[1000];
+	uint32_t startTime = clock(), elapsedTime = 0;
+	uint64_t elqualsCounter = 0, differentsCounter = 0, printMarkCounter = 0;
 
-    for (uint16_t index = 0; index < 1000; ++index) {
-        foo[index] = index;
-        bar[index] = 999 - index;
-    }
+	for (uint16_t index = 0; index < 1000; ++index) {
+		// populating
+		foo[index] = index;
+		bar[index] = 999 - index;
+	}
 
-    for (uint64_t trillionCounter = 1; trillionCounter < 1000000000000; ++trillionCounter) {
-        for (uint16_t fooIndex = 0; fooIndex < 1000; ++fooIndex) { //quatrillion
-            for (uint16_t barIndex = 0; barIndex < 1000; ++barIndex) { //quintillion
-                if (foo[fooIndex] == bar[barIndex]) {
-                    ++comparisonsCounter;
-                }
-            }
-        }
+	for (uint64_t trillionCounter = 1; trillionCounter < 1000000000000; ++trillionCounter) {
+		for (uint16_t fooIndex = 0; fooIndex < 1000; ++fooIndex) { // quatrillion
+			for (uint16_t barIndex = 0; barIndex < 1000; ++barIndex) { // quintillion
+				if (foo[fooIndex] == bar[barIndex]) {
+					++elqualsCounter;
+				} else { // this is to force the compiler to not optimize this logic
+					++differentsCounter;
+				}
+			}
+		}
 
-	// Consider that for each iteration of trillionCounter
-	//there are 1.000.000 more iterations over the comparisons between foo and bar.
-	// This is visible in conparisonsCounter.
-        if (trillionCounter % 10000 == 0) { // this means 10.000.000.000 of comparisons
-            ++coutMarkCounter;
-            std::cout << "comparisonsCounter: " << comparisonsCounter << std::endl;
-            elapsedTime = ((clock() - startTime) / 1000);
-            std::cout <<
-                "After a 100 billion comparisons, the elapsed time is: " <<
-                elapsedTime <<
-                " seconds.\nThis means that whole this should take about: " <<
-                ((((elapsedTime * 2500000) / 60) / 60) / 24) / coutMarkCounter <<
-		// 2500000 because 2.500.000 * 100.000.000.000 is 2.5^18
-                " days to finish in your machine!\n(this is: " <<
-                (((((elapsedTime * 2500000) / 60) / 60) / 24) / 365) / coutMarkCounter <<
-                " years!!\nNow split this number by the number of threads you have" <<
-                " and realize the it's almost impossible to calculate with CPU...\n" <<
-                "My best regards and thanks for testing!\n\n\n" <<
-            std::endl;
-        }
-    }
+		// Consider that for each iteration of trillionCounter
+		//there are 1.000.000 more iterations over the comparisons between foo and bar.
+		// This is visible in conparisonsCounter.
+		if (trillionCounter % 10000 == 0) { // this means 10.000.000.000 of comparisons
+			++printMarkCounter;
+			std::cout << "comparisons: " << elqualsCounter + differentsCounter << std::endl;
+			elapsedTime = ((clock() - startTime) / 1000);
+			std::cout <<
+				"After a 10 billion comparisons, the elapsed time is: " <<
+				elapsedTime <<
+				" seconds.\nThis means that whole this should take about: " <<
+				((((elapsedTime * 250000000) / 60) / 60) / 24) / printMarkCounter <<
+				// 2500000 because 250.000.000 * 10.000.000.000 is 2.5^18
+				" days to finish in your machine!\n(this is: " <<
+				(((((elapsedTime * 250000000) / 60) / 60) / 24) / 365) / printMarkCounter <<
+				" years!!\nNow split this number by the number of threads you have" <<
+				" and realize the it's almost impossible to calculate with CPU...\n" <<
+				"My best regards and thanks for testing!\n\n\n" <<
+			std::endl;
+		}
+	}
 }
