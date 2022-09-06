b238b929da58c5e0203455273fb26d4e39852349
Girzo <52513552+Giunossauro@users.noreply.github.com>
2022-09-06 20:16:14 -0300

    🔨 Refactoring

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 
 int main() {
 	uint16_t foo[1000], bar[1000];
-	uint32_t startTime = clock(), elapsedTime = 0;
-	uint64_t elqualsCounter = 0, differentsCounter = 0, printMarkCounter = 0;
+	clock_t startTime = clock(), elapsedTime;
+	uint64_t elqualsCounter = 0, differentsCounter = 0;
 
 	for (uint16_t index = 0; index < 1000; ++index) {
 		// populating
@@ -27,21 +27,21 @@ int main() {
 		//there are 1.000.000 more iterations over the comparisons between foo and bar.
 		// This is visible in conparisonsCounter.
 		if (trillionCounter % 10000 == 0) { // this means 10.000.000.000 of comparisons
-			++printMarkCounter;
 			std::cout << "comparisons: " << elqualsCounter + differentsCounter << std::endl;
-			elapsedTime = ((clock() - startTime) / 1000);
+			elapsedTime = ((clock() - startTime) / CLOCKS_PER_SEC);
 			std::cout <<
-				"After a 10 billion comparisons, the elapsed time is: " <<
-				elapsedTime <<
+				"After a " << elqualsCounter + differentsCounter <<
+				" comparisons, the elapsed time after the last print is: " << elapsedTime <<
 				" seconds.\nThis means that whole this should take about: " <<
-				((((elapsedTime * 250000000) / 60) / 60) / 24) / printMarkCounter <<
+				(((elapsedTime * 250000000) / 60) / 60) / 24 <<
 				// 2500000 because 250.000.000 * 10.000.000.000 is 2.5^18
 				" days to finish in your machine!\n(this is: " <<
-				(((((elapsedTime * 250000000) / 60) / 60) / 24) / 365) / printMarkCounter <<
-				" years!!\nNow split this number by the number of threads you have" <<
+				((((elapsedTime * 250000000) / 60) / 60) / 24) / 365 <<
+				" years!!)\nNow split this number by the number of threads you have" <<
 				" and realize the it's almost impossible to calculate with CPU...\n" <<
 				"My best regards and thanks for testing!\n\n\n" <<
 			std::endl;
+			startTime = clock();
 		}
 	}
 }
